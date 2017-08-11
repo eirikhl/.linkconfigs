49dc55507da42730ac579a4254ef2e188f489040
eirikhl <eirikhl@stud.ntnu.no>
2017-08-11 15:52:30 +0200

    Moved stuff around, nothing new, really. More stuff in bash script

--- always_do.sh
+++ always_do.sh
@@ -3,8 +3,8 @@
 # Pull the dotfiles repo
 cd ~/.linkconfigs && git pull --rebase
 
-# Set Caps Lock to Ctrl
-setxkbmap -option ctrl:nocaps
+# Norwegian Dvorak as default
+setxkbmap no -variant dvorak
 # Fix key bindings, swap Caps Lock and Escape
 xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock"
 xmodmap -e "keycode 66 = Escape NoSymbol Escape"
