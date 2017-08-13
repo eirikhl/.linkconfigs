db40a30bf10fc00b9f59e182570318ba219e29aa
eirikhl <eirikhl@stud.ntnu.no>
2017-08-13 21:10:59 +0200

    Xscreensaver at home, moved it to always_do

--- always_do.sh
+++ always_do.sh
@@ -9,5 +9,7 @@ setxkbmap no -variant dvorak
 xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock"
 xmodmap -e "keycode 66 = Escape NoSymbol Escape"
 
+exec xscreensaver &
+
 # Activate NumLock
 cd / && ./usr/bin/numlockx on
