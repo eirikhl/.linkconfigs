4797ae0fdfa8f4713ba0483258712099e64e0d47
eirikhl <eirikhl@stud.ntnu.no>
2017-08-14 15:27:03 +0200

    Fixed keyboard rebindings for caps lock

--- always_do.sh
+++ always_do.sh
@@ -6,6 +6,7 @@ cd ~/.linkconfigs && git pull --rebase
 # Norwegian Dvorak as default
 setxkbmap no -variant dvorak
 # Fix key bindings, swap Caps Lock and Escape
+setxkbmap -option ctrl:nocaps
 xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock"
 xmodmap -e "keycode 66 = Escape NoSymbol Escape"
 
