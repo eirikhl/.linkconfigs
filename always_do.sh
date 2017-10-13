64e6718cb09ea4f736de4dfca9a3a2e2c8e5dcde
eirikhl <eirikhl@stud.ntnu.no>
2017-10-13 15:45:03 +0200

    Mini fix for keyboard stuff

--- always_do.sh
+++ always_do.sh
@@ -3,10 +3,10 @@
 # Norwegian Dvorak as default
 setxkbmap no -variant dvorak
 # Fix key bindings, swap Caps Lock and Escape
+setxkbmap -option kpdl:dot
 setxkbmap -option ctrl:nocaps
 xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock"
 xmodmap -e "keycode 66 = Escape NoSymbol Escape"
-setxkbmap -option '' -option kpdl:dot
 # Activate NumLock
 numlockx on
 
