3c7f32f1211165920222eaf2ed1b27735791b767
eirikhl <eirikhl@stud.ntnu.no>
2018-04-23 04:39:54 +0200

    ibus

--- always_do.sh
+++ always_do.sh
@@ -7,6 +7,8 @@ setxkbmap -option kpdl:dot
 setxkbmap -option ctrl:nocaps
 xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock"
 xmodmap -e "keycode 66 = Escape NoSymbol Escape"
+# Keyboard widget thing
+exec ibus-daemon
 # Activate NumLock
 numlockx on
 
