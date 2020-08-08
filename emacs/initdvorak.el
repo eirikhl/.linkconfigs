cb1dc5389dd4abcff4cd42b151afc40ed8a4119e
eirikhl <eirikharald@hotmail.com>
2020-08-08 19:55:22 +0200

    Finally fixed my damn init.el. However, quitting is still bad

--- emacs/initdvorak.el
+++ emacs/initdvorak.el
@@ -1,16 +1,40 @@
+;;;; Finally not fucking up my fonts?
+(add-to-list 'default-frame-alist
+                       '(font . "DejaVu Sans Mono-10"))
+
+;;;; In case of future annoyances:
+;;;  Change 'allow-unsigned to nil
+;;;  Run M-x package-install RET gnu-elpa-keyring-update
+;;;  Change back
+(setq package-check-signature 'allow-unsigned)
+
+
 ;;;; Debug
 (setq debug-on-error t)
 (setq debug-on-quit t)
+;;;; Various keybindings I think make sense
+;;; Important note! C-m == enter/return
+(global-unset-key (kbd "C-|"))
+
 
 ;;;; Package stuff
-;; Melpa = package managing and stuff
+
+;;; Manually included libraries
+;; Directory for arbitrary libraries
+(add-to-list 'load-path "~/.emacs.d/libs")
+
+; Make sure to download the following libraries manually
+(load-library "whole-line-or-region")
+(whole-line-or-region-mode 1)
+(load-library "s")
+;(load-library "perl-completion") requires "anything.el", not bothering yet
+
+;;; Melpa = package managing and stuff
 (package-initialize nil)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-;(unless (assoc-default "melpa" package-archives)
-;  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-;  (package-refresh-contents))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 (require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (setq
  package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                     ("org" . "http://orgmode.org/elpa/")
@@ -18,48 +42,42 @@
                     ("melpa-stable" . "http://stable.melpa.org/packages/"))
  package-archive-priorities '(("melpa-stable" . 1)))
 
-;; Install missing packages
-; Guess it doesn't work like I want it to?
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+
+ ;; Install missing packages
+ (unless (package-installed-p 'use-package)
+   (package-refresh-contents)
+   (package-install 'use-package))
 (setq use-package-verbose t)
 (eval-when-compile
-  (require 'use-package))
+ (require 'use-package))
 (setq use-package-always-ensure t)
 ;; Auto-complete thingamajig
 (unless (package-installed-p 'auto-complete)
-  (package-refresh-contents)
-  (package-install 'auto-complete))
+ (package-refresh-contents)
+ (package-install 'auto-complete))
 (ac-config-default)
-;; Automatically install base16
-(unless (package-installed-p 'base16-theme)
-  (package-refresh-contents)
-  (package-install 'base16-theme))
-;; Automatically install swiper
-(unless (package-installed-p 'swiper)
-  (package-refresh-contents)
-  (package-install 'swiper))
+
 ;; Ivy does fancy shit
 (use-package ivy)
 (ivy-mode 1)
+(use-package ivy :demand
+  :config
+  (setq ivy-use-virtual-buffers t
+	ivy-count-format "%d/%d "))
 (setq ivy-use-virtual-buffers t)
 (setq enable-recursive-minibuffers t)
+
+;; Automatically install swiper
+(unless (package-installed-p 'swiper)
+ (package-refresh-contents)
+ (package-install 'swiper))
 (global-set-key (kbd "C-o") 'swiper)
 
 
-;;; Manually included libraries
-;; Directory for arbitrary libraries
-(add-to-list 'load-path "~/.emacs.d/libs")
-
-(load-library "whole-line-or-region")
-(whole-line-or-region-mode 1)
-
-;; Loadsa stringy stuff
-(load-library "s")
-
 ;;;; Various keybindings I think make sense
 ;;; Important note! C-m == enter/return
+(global-unset-key (kbd "C-|"))
+
 ;; Macro to set a list of keybindings and functions
 (defmacro gsk (l)
   `(progn ,@(mapcar (lambda (l)
@@ -83,7 +101,8 @@
       ("C-q C-u" 'find-file)
       ("C-q C-j" 'save-buffers-kill-emacs) ; exit emacs
       ("M-q" 'execute-extended-command) ; map M-q to M-x
-      ("<escape> C-å" 'keyboard-escape-quit)
+      ("<escape> <escape>" 'ivy-immediate-done)
+      ("C-g" 'top-level)
       ("C-M-s" 'magit-status)
       ("C-h" 'previous-line)
       ("C-t" 'next-line)
@@ -95,25 +114,22 @@
       ("C-z" 'move-end-of-line)
       ("C-M-<left>" 'shrink-window-horizontally)
       ("C-M-<right>" 'enlarge-window-horizontally)
+      ("M-|" 'other-window)
+      ("C-q C-u" 'counsel-find-file)
       ))
 
-;; Fix js-mode fucking up my keybindings
-(eval-after-load 'js
-  '(define-key js-mode-map (kbd "M-.") 'next-buffer))
+
+
 
 ;;;; Convenience and quality of life, random stuff in general
-;; Spell-checking
-(setq ispell-program-name "aspell") ; could be ispell as well, depending on your preferences
-(setq ispell-dictionary "english") ; this can obviously be set to any language your spell-checking program supports
-(add-hook 'LaTeX-mode-hook 'flyspell-mode)
-(add-hook 'LaTeX-mode-hook 'flyspell-buffer)
-
-;; Collapse parts of the text
-(defun turn-on-outline-minor-mode ()
-(outline-minor-mode 1))
-(add-hook 'LaTeX-mode-hook 'turn-on-outline-minor-mode)
-(add-hook 'latex-mode-hook 'turn-on-outline-minor-mode)
-(setq outline-minor-mode-prefix "\C-c \C-g") ; Or something else
+;; Autocomplete
+;; dirty fix for having AC everywhere
+(define-globalized-minor-mode real-global-auto-complete-mode
+  auto-complete-mode (lambda ()
+                     (if (not (minibufferp (current-buffer)))
+                     (auto-complete-mode 1))
+                     ))
+(real-global-auto-complete-mode t)
 
 ;; Smooth scrolling
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
@@ -168,7 +184,7 @@
 ;; Uncomment if curly braces won't close in .R files
 ;; https://github.com/emacs-ess/ESS/issues/296#issuecomment-189614821
 ;;(define-key ess-mode-map (kbd "{") nil)
-;;(define-key ess-mode-map (kbd "}") nil) 
+;;(define-key ess-mode-map (kbd "}") nil)
 (global-set-key (kbd "(") 'skeleton-pair-insert-maybe)
 (global-set-key (kbd "[") 'skeleton-pair-insert-maybe)
 (global-set-key (kbd "{") 'skeleton-pair-insert-maybe)
@@ -207,7 +223,6 @@
 ;; Unholy abomination which should display total line number
 (defvar my-mode-line-buffer-line-count nil)
 (make-variable-buffer-local 'my-mode-line-buffer-line-count)
-
 (setq-default mode-line-format
               '("  " mode-line-modified
                 (list 'line-number-mode "  ")
@@ -226,6 +241,7 @@
 (add-hook 'after-save-hook 'my-mode-line-count-lines)
 (add-hook 'after-revert-hook 'my-mode-line-count-lines)
 (add-hook 'dired-after-readin-hook 'my-mode-line-count-lines)
+
 
 ;; "Smart"/IntelliJ-style Ctrl-backspace
 (defun aborn/backward-kill-word ()
@@ -263,16 +279,17 @@
 (global-set-key  [C-backspace]
             'aborn/backward-kill-word)
 
+
 ;; C++-mode indentation
 (defun my-c-mode-common-hook ()
   ;; my customizations for all of c-mode, c++-mode, objc-mode, java-mode
   (c-set-offset 'substatement-open 0)
   ;; other customizations can go here
-  
+
   (setq c++-tab-always-indent t)
   (setq c-basic-offset 4)                  ;; Default is 2
   (setq c-indent-level 4)                  ;; Default is 2
-  
+
   (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
   (setq tab-width 4)
   (setq indent-tabs-mode t)  ; use spaces only if nil
@@ -287,48 +304,14 @@
 
 
 ;;;; Visual stuff
-;; Change default colour theme
-;; Different ones I think look kinda nice
-; (load-theme 'base16-isotope t)
-; (load-theme 'tango-dark)
-; (load-theme 'base16-railscasts)
-; (load-theme 'adwaita)
-
-;(use-package spacemacs-theme
-;  :ensure t
-;  :init
-;  (load-theme 'spacemacs-dark t)
-;  (setq spacemacs-theme-org-agenda-height nil)
-;  (setq spacemacs-theme-org-height nil))
 (use-package spacemacs-theme
-  :ensure spacema
-  :defer spacemacs-theme
+  :defer t
   :init (load-theme 'spacemacs-dark t))
 
-(use-package spaceline
-  :ensure spaceline
-  :demand t
-  :init
-  (setq powerline-default-separator 'arrow-fade)
-  :config
-  (require 'spaceline-config)
-  (spaceline-spacemacs-theme))
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(tool-bar-mode 0)
 
-;; Change cursor and line highlighting
-(set-default 'cursor-type 'bar)
-;; (global-hl-line-mode 1)
-
-;; Highlight parentheses
-(show-paren-mode 1)
-(setq show-paren-delay 0)
-
-;; Highlight numbers and function calls
-;; (add-hook 'prog-mode-hook 'highlight-numbers-mode)
-;; (add-hook 'prog-mode-hook 'highlight-function-calls-mode)
-
-
-;; Disable scroll bar
-(scroll-bar-mode -1)
 ;; Display line numbers
 (global-linum-mode t)
 ;; Disable startup message
@@ -339,23 +322,35 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
  '(ansi-color-names-vector
-   ["#f5f7ff" "#c94922" "#ac9739" "#c08b30" "#3d8fd1" "#6679cc" "#3d8fd1" "#5e6687"])
- '(ansi-term-color-vector
-   [unspecified "#f5f7ff" "#c94922" "#ac9739" "#c08b30" "#3d8fd1" "#6679cc" "#3d8fd1" "#5e6687"])
+   ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0" "#b2b2b2"])
  '(custom-safe-themes
    (quote
-    ("a85e40c7d2df4a5e993742929dfd903899b66a667547f740872797198778d7b5" "8be07a2c1b3a7300860c7a65c0ad148be6d127671be04d3d2120f1ac541ac103" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "eae831de756bb480240479794e85f1da0789c6f2f7746e5cc999370bbc8d9c8a" "04790c9929eacf32d508b84d34e80ad2ee233f13f17767190531b8b350b9ef22" default)))
- '(menu-bar-mode nil)
- '(tool-bar-mode nil))
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(hl-todo-keyword-faces
+   (quote
+    (("TODO" . "#dc752f")
+     ("NEXT" . "#dc752f")
+     ("THEM" . "#2d9574")
+     ("PROG" . "#4f97d7")
+     ("OKAY" . "#4f97d7")
+     ("DONT" . "#f2241f")
+     ("FAIL" . "#f2241f")
+     ("DONE" . "#86dc2f")
+     ("NOTE" . "#b1951d")
+     ("KLUDGE" . "#b1951d")
+     ("HACK" . "#b1951d")
+     ("TEMP" . "#b1951d")
+     ("FIXME" . "#dc752f")
+     ("XXX+" . "#dc752f")
+     ("\\?\\?\\?+" . "#dc752f"))))
+ '(package-selected-packages
+   (quote
+    (ivy counsel magit gnu-elpa-keyring-update use-package)))
+ '(pdf-view-midnight-colors (quote ("#b2b2b2" . "#292b2e"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack" :foundry "simp" :slant normal :weight normal :height 83 :width normal))))
- '(hl-line ((t (:background "dim gray")))))
-
-
+ )
